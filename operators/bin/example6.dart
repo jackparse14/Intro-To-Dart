54afc872d0a7dde0c237bc841466c30272ffa382
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-10 21:39:01 +0100

    finished operators chapter

---
+++ operators/bin/example6.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  // print("Hello" + 2);
+  var myAge = 20;
+  print(myAge = 30);
+  print(myAge);
+  print(myAge ~/= 2); // compound assignment operator
+  print(myAge);
+
+  print(myAge *= 2);
+  print(myAge += 2);
+  print(myAge -= 2);
+  print(myAge &= 12612);
+  print(myAge |= 10);
+  print(myAge ^= 2);
+}
