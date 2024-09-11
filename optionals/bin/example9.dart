e55e56a11d7cf3b839054bad77c1477c4e101f87
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 15:32:26 +0100

    example9 optionals

--- optionals/bin/example9.dart
+++ optionals/bin/example9.dart
@@ -1,1 +1,14 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  String? lastName;
+  void changeLastName() {
+    lastName = "Bar";
+  }
+
+  changeLastName();
+  if (lastName?.contains("Bar") ?? false) {
+    print(lastName);
+  }
+  if (lastName?.contains("Bar") == true) {
+    print(lastName);
+  }
+}
