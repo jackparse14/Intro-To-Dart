30a4a87e036860fec407e13f1cb94af2d0bacb7a
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 15:58:36 +0100

    example11 optionals

--- optionals/bin/example11.dart
+++ optionals/bin/example11.dart
@@ -1,1 +1,24 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  print(getFullName(null, null));
+  print(getFullName("John", null));
+  print(getFullName(null, "Doe"));
+  print(getFullName("John", "Doe"));
+}
+
+String getFullName(
+  String? firstName,
+  String? lastName,
+) =>
+    withAll(
+      [firstName, lastName], // optionals
+      (names) => names.join(" "), // callback
+    ) ??
+    "Empty";
+
+// Generic function
+T? withAll<T>(
+  List<T?> optionals,
+  T Function(List<T>) callback,
+) =>
+    optionals.any((e) => e == null) ? null : callback(optionals.cast<T>());
+    // If any optional = null return null else run callback with list of optionals
