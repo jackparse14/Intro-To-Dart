3f1ea6a47ab33725770ec31262e80a0f6bc76c05
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 14:51:57 +0100

    example4 optionals

--- optionals/bin/example4.dart
+++ optionals/bin/example4.dart
@@ -1,1 +1,10 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  String? lastName;
+  lastName = "Baz";
+  lastName ??= "Bar"; // compound assignment operator / If null assign value
+  print(lastName);
+}
+
+void doSomething(String? one, String? two) {
+  one ??= two;
+}
