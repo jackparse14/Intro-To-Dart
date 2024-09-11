2783028ff457be4cdc943e5f4eca725008f50457
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 15:12:38 +0100

    example6 optionals

--- optionals/bin/example6.dart
+++ optionals/bin/example6.dart
@@ -1,1 +1,17 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  List<String?>? names;
+  names?.add("Foo");
+  names?.add(null);
+  print(names);
+
+  // List<String> names2 = [];
+  // final bla = names2.first;
+
+  final String? first = names?.first;
+  print(first ?? "No first name found");
+
+  names = [];
+  names.add("Baz");
+  names.add(null);
+  print(names);
+}
