41c293426711e717e7eed622f101380861572ce5
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 16:19:40 +0100

    example12 optionals

--- optionals/bin/example12.dart
+++ optionals/bin/example12.dart
@@ -1,1 +1,27 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  String? firstName = "John";
+  String? lastName = "Doe";
+
+  final fullName = firstName.flatMap(
+        (f) => lastName.flatMap(
+          (l) => '$f $l',
+        ),
+      ) ??
+      "Either first or last name or both are null";
+  print(fullName);
+}
+
+extension FlatMap<T> on T? {
+  R? flatMap<R>(
+    R? Function(T) callback,
+  ) {
+    final shadow = this;
+    if (shadow == null) {
+      return null;
+    } else {
+      return callback(shadow);
+    } // if the variable that calls flatmap is null it returns null otherwise it returns the result of the callback provided with itself as the argument
+  }
+}
+
+// T and R are both generic types, they do the same thing they are just naming conventions for having multiple generic types
