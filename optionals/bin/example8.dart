7e4184fdc2bbbbf0c745ce23e984c44ad7e2580a
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 15:25:36 +0100

    example8 optionals

--- optionals/bin/example8.dart
+++ optionals/bin/example8.dart
@@ -1,1 +1,10 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final String? firstName = null;
+  final int? length = firstName?.length;
+  if (firstName == null) {
+    print("first name is null");
+  } else {
+    print(firstName.length);
+    final length = firstName?.length;
+  }
+}
