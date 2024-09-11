f88ebaf44ec8fb63aeacd1f135ba46a52f21ee95
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 15:00:58 +0100

    example5 optionals

--- optionals/bin/example5.dart
+++ optionals/bin/example5.dart
@@ -1,1 +1,9 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  String? lastName;
+  print(lastName?.length);
+
+  String? nullName;
+  print(lastName ?? "foo");
+  print(lastName ?? nullName);
+  print(lastName ?? nullName ?? "Bar");
+}
