fcd3d2bffc538ddfb1c8e1defb5ed9bb21aa81be
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-11 15:19:49 +0100

    example7 optionals

--- optionals/bin/example7.dart
+++ optionals/bin/example7.dart
@@ -1,1 +1,8 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  try {
+    final String? firstName = null;
+    print(firstName!); // dont use this null check operator
+  } catch (error) {
+    print(error);
+  }
+}
