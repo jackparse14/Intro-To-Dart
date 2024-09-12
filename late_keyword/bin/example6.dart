4d0d01fd1bfa1b9656cb0374c078b9c01ad79a2e
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-12 09:32:16 +0100

    example6 late keyword chapter

--- late_keyword/bin/example6.dart
+++ late_keyword/bin/example6.dart
@@ -1,1 +1,19 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final person = Person();
+  try {
+    // poor code we should use optionals instead
+    print(person.fullName);
+  } catch (e) {
+    print(e);
+  }
+
+  person.firstName = "Foo";
+  person.lastName = "Bar";
+  print(person.fullName);
+}
+
+class Person {
+  late final String firstName;
+  late final String lastName;
+  late String fullName = '$firstName $lastName';
+}
