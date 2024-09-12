93defa2f83cf8c8d2d3eaa2a92854b4220dd5b04
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-12 09:47:29 +0100

    example7 late keyword chapter

--- late_keyword/bin/example7.dart
+++ late_keyword/bin/example7.dart
@@ -1,1 +1,16 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  late final bool isTeenager;
+  const age = 12;
+  if (age >= 13 && age <= 19) {
+    isTeenager = true;
+  } else if (age < 13) {
+    isTeenager = false;
+  }
+  try {
+    // try to avoid try catches with late variables
+    isTeenager = false;
+    print("isTeenager = $isTeenager");
+  } catch (e) {
+    print(e);
+  }
+}
