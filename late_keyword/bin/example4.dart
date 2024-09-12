0fb950a189b9fb1b9bc05a834e7da3518565fb22
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-12 09:16:57 +0100

    example4 late keyword chapter

--- late_keyword/bin/example4.dart
+++ late_keyword/bin/example4.dart
@@ -1,1 +1,22 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  final person = Person();
+  print(person.fullName); // function is only called once
+  print(person.fullName);
+  print(person.fullName);
+
+  print(person.firstName);
+  print(person.lastName);
+}
+
+class Person {
+  late String fullName = _getFullName();
+  late String firstName = fullName
+      .split(' ')
+      .first; // chain of late variables dependent on eachother
+  late String lastName = fullName.split(' ').last;
+  String _getFullName() {
+    // private function
+    print("_getFullName() is called");
+    return "Foo Bar";
+  }
+}
