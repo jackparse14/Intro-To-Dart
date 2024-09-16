c9aff1c168ab1b1bf058f1d9fa2ccbc0a7e5e958
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-16 20:44:58 +0100

    example1 functions

--- functions/bin/example1.dart
+++ functions/bin/example1.dart
@@ -1,3 +1,8 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  printWow();
+}
 
+void printWow() {
+  print("Wow");
+}
 // nodemon --watch bin --ext dart --exec "cls && dart run bin/example1.dart"
