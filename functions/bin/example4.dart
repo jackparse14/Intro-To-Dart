e071427f38beb939a9a274211aa5b009ff4f8a38
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-18 08:39:14 +0100

    example4 functions

---
+++ functions/bin/example4.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  greet();
+
+  // you would never invoke the void function and assign the result to a variable
+  // final void value = greet();
+}
+
+void greet() {
+  print("Greetings!");
+}
