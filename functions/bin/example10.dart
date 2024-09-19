8149a189621524be6f161dd1a5710029f6a0e9bd
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-19 19:10:52 +0100

    example10 functions

--- functions/bin/example10.dart
+++ functions/bin/example10.dart
@@ -1,1 +1,12 @@
-void main(List<String> args) {}
+void main(List<String> args) {
+  describePerson();
+  describePerson(age: 20);
+  describePerson(name: 'Foo');
+  // The order of parameters do not matter
+  describePerson(name: 'Foo', age: 20);
+  describePerson(age: 20, name: 'Foo');
+}
+
+void describePerson({String? name, int? age}) {
+  print('Hello $name, you are $age years old.');
+}
