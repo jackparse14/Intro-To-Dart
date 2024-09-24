d847635f59a43f761defd6f7200344e78985261d
Jack Parsons <jackparse14@hotmail.co.uk>
2024-09-24 16:19:43 +0100

    example10 collections

---
+++ collections/bin/example10.dart
@@ -0,0 +1,62 @@
+import 'dart:collection';
+
+void main(List<String> args) {
+  const notFound = 'NOT_FOUND';
+  const defaultString = 'DEFAULT_VALUE';
+
+  final myList = SafeList(
+      defaultValue: defaultString,
+      absentValue: notFound,
+      values: ['bar', 'baz']);
+
+  print(myList[0]);
+  print(myList[1]);
+  print(myList[2]);
+
+  myList.length = 4;
+  print(myList[3]);
+  print(myList);
+
+  myList.length = 0;
+  print(myList.first);
+  print(myList.last);
+}
+
+class SafeList<T> extends ListBase<T> {
+  final T absentValue;
+  final T defaultValue;
+  final List<T> _list;
+
+  SafeList({
+    required this.defaultValue,
+    required this.absentValue,
+    List<T>? values,
+  }) : _list = values ?? [];
+
+  @override
+  int get length => _list.length;
+
+  @override
+  set length(int newLength) {
+    if (newLength < _list.length) {
+      _list.length = newLength;
+    } else {
+      _list.addAll(List.filled(
+        newLength - _list.length,
+        defaultValue,
+      ));
+    }
+  }
+
+  @override
+  T get first => _list.isNotEmpty ? _list.first : absentValue;
+
+  @override
+  T get last => _list.isNotEmpty ? _list.last : absentValue;
+
+  @override
+  T operator [](int index) => index < _list.length ? _list[index] : absentValue;
+
+  @override
+  void operator []=(int index, T value) => _list[index] = value;
+}
